suraj-sriwastava/pal-tracker

Add Steeltoe Management endpoints

src/PalTracker/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -42,6 +46,14 @@ namespace PalTracker
             services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
 
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+
+            services.AddCloudFoundryActuators(Configuration);
+
+            services.AddSingleton<IHealthContributor, TimeEntryHealthContributor>();
+
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +70,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCloudFoundryActuators();
         }
     }
 }

sniemeyer13/pal-tracker-dotnet

src/PalTracker/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -42,6 +46,14 @@ namespace PalTracker
             services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
 
             services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+
+            services.AddCloudFoundryActuators(Configuration);
+
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +70,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseCloudFoundryActuators();
         }
     }
 }